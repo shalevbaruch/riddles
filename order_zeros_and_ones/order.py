c3d2ee2f3526f3201c16cba87773c3ea740c2a12
shalevbaruch <100311710+shalevbaruch@users.noreply.github.com>
2023-05-12 15:06:32 +0300

    Update order.py

--- order_zeros_and_ones/order.py
+++ order_zeros_and_ones/order.py
@@ -1,1 +1,25 @@
+def order(arr):
+    left_index = len(arr) - 1
+    right_index = 0
+    if len(arr) <= 1:
+        return arr
+    
+    while left_index > right_index:
+        while (left_index >= 0) and arr[left_index] == 1:
+            left_index -= 1
+        while (right_index <= len(arr)-1) and arr[right_index] == 0:
+            right_index += 1
+        if left_index > right_index:
+            switch(arr, left_index, right_index)
+            left_index -= 1
+            right_index += 1
+    return arr
 
+
+def switch(arr, left_index, right_index):
+        temp = arr[left_index]
+        arr[left_index] = arr[right_index]
+        arr[right_index] = temp
+
+
+        
